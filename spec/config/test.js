fa2770177ab4c2097215e24cada535200b3c2e9c
Arthur Cinader <arthur@sugarinc.com>
2016-12-06 14:52:57 -0800

    Handle immutable configuration (#33)
    * Handle immutable configuration
    
    The popular and awesome https://github.com/lorenwest/node-config
    makes all configs values immutable.  Good practice!
    
    parse-server-s3-adapter attempts to edit config values.
    
    This fix will respect immutability allowing this adapter
    to work with node-config.
    
    * PR comments....
    
    1. move setting NODE_ENV to test command instead of helpers.
    2. add a missing ;.  lint coming.....

---
+++ spec/config/test.js
@@ -0,0 +1,18 @@
+module.exports = {
+  accessKey: 'accessKey',
+  secretKey: 'secretKey',
+  insufficientOptions: {
+    accessKey: 'accessKey',
+    secretKey: 'secretKey',
+  },
+  bucket: 'bucket',
+  objectWithBucket: {
+      bucket: 'bucket',
+  },
+  emptyObject: {},
+  paramsObjectWBucket: {
+      params: {
+          Bucket: 'bucket',
+      },
+  },
+};
