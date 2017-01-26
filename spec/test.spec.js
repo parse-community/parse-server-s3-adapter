078a3429f349b090537f49636a0279906ec6feb8
Marc Smith <mr.marcsmith@me.com>
2017-01-26 13:18:46 -0700

    added tests for SOverrides as Env or an option

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -132,6 +132,35 @@ describe('S3Adapter tests', () => {
       expect(s3._s3Client.config.params.Bucket).toEqual('bucket-2');
       expect(s3._bucketPrefix).toEqual('test/');
     });
+    it('should accept options and overrides as an option in args', () => {
+      var confObj = { bucketPrefix: 'test/', bucket: 'bucket-1', secretKey: 'secret-1', accessKey: 'key-1' , s3overrides: { secretAccessKey: 'secret-2', accessKeyId: 'key-2', params: { Bucket: 'bucket-2' }} };
+      var s3 = new S3Adapter(confObj);
+      expect(s3._s3Client.config.accessKeyId).toEqual('key-2');
+      expect(s3._s3Client.config.secretAccessKey).toEqual('secret-2');
+      expect(s3._s3Client.config.params.Bucket).toEqual('bucket-2');
+      expect(s3._bucketPrefix).toEqual('test/');
+    });
+
+    it('should accept options and overrides as an Enviromental Variable', () => {
+      var confObj = { bucketPrefix: 'test/', bucket: 'bucket-1', secretKey: 'secret-1', accessKey: 'key-1' };
+      process.env.S3_OVERRIDES = { secretAccessKey: 'secret-2', accessKeyId: 'key-2', params: { Bucket: 'bucket-2' }};
+      var s3 = new S3Adapter(confObj);
+      expect(s3._s3Client.config.accessKeyId).toEqual('key-2');
+      expect(s3._s3Client.config.secretAccessKey).toEqual('secret-2');
+      expect(s3._s3Client.config.params.Bucket).toEqual('bucket-2');
+      expect(s3._bucketPrefix).toEqual('test/');
+    });
+
+    it('should accept overrides without params', () => {
+      var confObj = { bucketPrefix: 'test/', bucket: 'bucket-1', secretKey: 'secret-1', accessKey: 'key-1' };
+      var overridesObj = { secretAccessKey: 'secret-2'};
+      var s3 = new S3Adapter(confObj, overridesObj);
+      expect(s3._s3Client.config.accessKeyId).toEqual('key-1');
+      expect(s3._s3Client.config.secretAccessKey).toEqual('secret-2');
+      expect(s3._s3Client.config.params.Bucket).toEqual('bucket-1');
+      expect(s3._bucketPrefix).toEqual('test/');
+    });
+  });
   });
 
   describe('getFileLocation', () => {
