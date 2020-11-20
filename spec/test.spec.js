21e4d9c947b65ad6c5271e870f21f6be49467d6a
Daniel San <daniel.samrocha@gmail.com>
2020-11-19 23:55:18 -0300

    Add tests for presigned url
    Signed-off-by: Daniel San <daniel.samrocha@gmail.com>

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -325,6 +325,7 @@ describe('S3Adapter tests', () => {
 
       beforeEach(() => {
         options = {
+          presignedUrl: false,
           directAccess: true,
           bucketPrefix: 'foo/bar/',
           baseUrl,
@@ -334,6 +335,20 @@ describe('S3Adapter tests', () => {
       it('should get using the baseUrl', () => {
         const s3 = new S3Adapter('accessKey', 'secretKey', 'my-bucket', options);
         expect(s3.getFileLocation(testConfig, 'test.png')).toEqual('http://example.com/files/foo/bar/test.png');
+      });
+
+      it('should get using the baseUrl and amazon using presigned URL', () => {
+        options.presignedUrl = true;
+        const s3 = new S3Adapter('accessKey', 'secretKey', 'my-bucket', options);
+
+        const fileLocation = s3.getFileLocation(testConfig, 'test.png');
+        expect(fileLocation).toMatch(/^http:\/\/example.com\/files\/foo\/bar\/test.png\?/);
+        expect(fileLocation).toMatch(/X-Amz-Credential=accessKey%2F\d{8}%2Fus-east-1%2Fs3%2Faws4_request/);
+        expect(fileLocation).toMatch(/X-Amz-Date=\d{8}T\d{6}Z/);
+        expect(fileLocation).toMatch(/X-Amz-Signature=.{64}/);
+        expect(fileLocation).toContain('X-Amz-Algorithm=AWS4-HMAC-SHA256');
+        expect(fileLocation).toContain('X-Amz-Expires=900');
+        expect(fileLocation).toContain('X-Amz-SignedHeaders=host');
       });
 
       it('should get direct to baseUrl', () => {
@@ -352,6 +367,21 @@ describe('S3Adapter tests', () => {
         delete options.baseUrl;
         const s3 = new S3Adapter('accessKey', 'secretKey', 'my-bucket', options);
         expect(s3.getFileLocation(testConfig, 'test.png')).toEqual('https://my-bucket.s3.amazonaws.com/foo/bar/test.png');
+      });
+
+      it('should go directly to amazon using presigned URL', () => {
+        delete options.baseUrl;
+        options.presignedUrl = true;
+        const s3 = new S3Adapter('accessKey', 'secretKey', 'my-bucket', options);
+
+        const fileLocation = s3.getFileLocation(testConfig, 'test.png');
+        expect(fileLocation).toMatch(/^https:\/\/my-bucket.s3.amazonaws.com\/foo\/bar\/test.png\?/);
+        expect(fileLocation).toMatch(/X-Amz-Credential=accessKey%2F\d{8}%2Fus-east-1%2Fs3%2Faws4_request/);
+        expect(fileLocation).toMatch(/X-Amz-Date=\d{8}T\d{6}Z/);
+        expect(fileLocation).toMatch(/X-Amz-Signature=.{64}/);
+        expect(fileLocation).toContain('X-Amz-Algorithm=AWS4-HMAC-SHA256');
+        expect(fileLocation).toContain('X-Amz-Expires=900');
+        expect(fileLocation).toContain('X-Amz-SignedHeaders=host');
       });
     });
   });
