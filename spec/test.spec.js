771b533e6345ab2272cad9aad503876a80351775
Vahid Sane <azizalizad@gmail.com>
2024-08-07 12:51:43 +0000

    add some tests inline to debug

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -643,5 +643,35 @@ describe('S3Adapter tests', () => {
     });
   });
 
+  it("should properly create, read, delete files remove later", (done) => {
+    const adapter = makeS3Adapter({});
+    var filename = 'file.txt';
+    adapter.createFile(filename, "hello world", 'text/utf8').then((result) => {
+      return adapter.getFileData(filename);
+    }, (err) => {
+      fail("The adapter should create the file");
+      done();
+    }).then((result) => {
+      expect(result instanceof Buffer).toBe(true);
+      expect(result.toString('utf-8')).toEqual("hello world");
+      return adapter.deleteFile(filename);
+    }, (err) => {
+      fail("The adapter should get the file");
+      done();
+    }).then((result) => {
+
+      adapter.getFileData(filename).then((res) => {
+        fail("the file should be deleted");
+        done();
+      }, (err) => {
+        done();
+      });
+
+    }, (err) => {
+      fail("The adapter should delete the file");
+      done();
+    });
+  }, 5000);
+
   filesAdapterTests.testAdapter('S3Adapter', makeS3Adapter({}));
 });
