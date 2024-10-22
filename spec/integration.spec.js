2270c56f6f7fb4c2f4f4d524a415c8c3f6745359
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2024-10-22 14:24:07 +0200

    clean up tests

--- spec/integration.spec.js
+++ spec/integration.spec.js
@@ -1,53 +1,37 @@
 const { httpRequest } = require('./support/request');
 
-const fileData = 'hello world';
-
 describe('S3Adapter integration tests', () => {
   it('should create a file in Parse Server', async () => {
-    const fileName = 'test-1.txt';
-
-    const base64 = Buffer.from(fileData).toString('base64');
-    const file = new Parse.File(fileName, { base64 });
-
+    const base64 = Buffer.from('1').toString('base64');
+    const file = new Parse.File('file.txt', { base64 });
     await file.save();
 
     expect(file).toBeDefined();
-    expect(file.url()).toContain(fileName);
+    expect(file.url()).toMatch(/file.txt$/);
   });
 
-  it(
-    'should read the contents of the file',
-    async () => {
-      const fileName = 'test-2.txt';
-      const base64 = Buffer.from(fileData).toString('base64');
-      const file = new Parse.File(fileName, { base64 });
-      await file.save();
-      const fileLink = file.url();
+  it('should read the contents of the file', async () => {
+    const base64 = Buffer.from('1').toString('base64');
+    const file = new Parse.File('file.txt', { base64 });
+    await file.save();
+    const fileLink = file.url();
 
-      const response = await httpRequest(fileLink);
-      const text = response.toString();
+    const response = await httpRequest(fileLink);
+    const text = response.toString();
 
-      expect(text).toBe(fileData); // Check if the contents match the original data
-    },
-    60 * 1000
-  );
+    expect(text).toBe('1');
+  });
 
-  it(
-    'should delete the file',
-    async () => {
-      const fileName = 'test-3.txt';
+  it('should delete the file', async () => {
+    const base64 = Buffer.from('1').toString('base64');
+    const file = new Parse.File('file.txt', { base64 });
+    await file.save();
 
-      const base64 = Buffer.from(fileData).toString('base64');
-      const file = new Parse.File(fileName, { base64 });
-      await file.save();
+    const fileLink = file.url();
+    await file.destroy();
 
-      const fileLink = file.url();
-      await file.destroy();
-
-      return expectAsync(httpRequest(fileLink)).toBeRejectedWithError(
-        'Request failed with status code 404'
-      );
-    },
-    60 * 1000
-  );
+    await expectAsync(httpRequest(fileLink)).toBeRejectedWithError(
+      'Request failed with status code 404'
+    );
+  });
 });
