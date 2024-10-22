c3fcc297fc16552595301b7b3f7a3212164999de
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2024-10-22 15:53:00 +0200

    add parse server matrix

---
+++ spec/support/jasmine.js
@@ -0,0 +1,11 @@
+const semver = require('semver');
+
+const satisfiesParseServerVersion = version => {
+  const envVersion = process.env.PARSE_SERVER_VERSION;
+  return !envVersion || semver.satisfies(envVersion, version);
+}
+
+global.it_only_parse_server_version = version => satisfiesParseServerVersion(version) ? it : xit;
+global.fit_only_parse_server_version = version => satisfiesParseServerVersion(version) ? fit : xit;
+global.describe_only_parse_server_version = version => satisfiesParseServerVersion(version) ? describe : xdescribe;
+global.fdescribe_only_parse_server_version = version => satisfiesParseServerVersion(version) ? fdescribe : xdescribe;
