5d39edd3820482fa88b64417c352c92dfc621197
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2024-10-22 22:03:49 +0200

    refactor mock

--- spec/mocks/s3adapter.js
+++ spec/mocks/s3adapter.js
@@ -3,74 +3,56 @@ const S3Adapter = require('../../index.js');
 const { GetObjectCommand, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 
 function getMockS3Adapter(options) {
-  let s3;
+  const accessKey = process.env.TEST_S3_ACCESS_KEY || 'ACCESS_KEY';
+  const secretKey = process.env.TEST_S3_SECRET_KEY || 'SECRET_KEY';
+  const bucket = process.env.TEST_S3_BUCKET || 'BUCKET';
 
-  if (
-    process.env.TEST_S3_ACCESS_KEY &&
-    process.env.TEST_S3_SECRET_KEY &&
-    process.env.TEST_S3_BUCKET
-  ) {
-    // Should be initialized from the env
-    s3 = new S3Adapter(
-      Object.assign(
-        {
-          accessKey: process.env.TEST_S3_ACCESS_KEY,
-          secretKey: process.env.TEST_S3_SECRET_KEY,
-          bucket: process.env.TEST_S3_BUCKET,
-          region: process.env.TEST_S3_REGION,
-        },
-        options
-      )
-    );
-  } else {
-    const bucket = 'FAKE_BUCKET';
-    const region = 'us-east-1';
+  const s3 = new S3Adapter(Object.assign({
+    accessKey,
+    secretKey,
+    bucket,
+  }, options));
 
-    s3 = new S3Adapter('FAKE_ACCESS_KEY', 'FAKE_SECRET_KEY', bucket, options);
+  const objects = {};
 
-    const objects = {};
+  s3._s3Client = {
+    // @ts-ignore
+    send: command => {
+      if (command instanceof PutObjectCommand) {
+        const { Key, Body } = command.input;
+        objects[Key] = Body;
+        return Promise.resolve({ Location: `https://${bucket}.s3.${region}.amazonaws.com/${Key}` });
+      }
 
-    s3._s3Client = {
-      send: command => {
-        if (command instanceof PutObjectCommand) {
-          const { Key, Body } = command.input;
+      if (command instanceof DeleteObjectCommand) {
+        const { Key } = command.input;
+        delete objects[Key];
+        return Promise.resolve({});
+      }
 
-          objects[Key] = Body;
+      if (command instanceof GetObjectCommand) {
+        const { Key } = command.input;
 
-          return Promise.resolve({
-            Location: `https://${bucket}.s3.${region}.amazonaws.com/${Key}`,
-          });
+        if (objects[Key]) {
+          const stream = new Readable();
+          stream.push('hello world');
+          // End of stream
+          stream.push(null);
+          return {
+            Body: stream,
+            AcceptRanges: 'bytes',
+            ContentLength: 36,
+            ContentRange: 'bytes 0-35/36',
+            ContentType: 'text/plain',
+          };
+        } else {
+          return Promise.reject(new Error('Not found'));
         }
-        if (command instanceof DeleteObjectCommand) {
-          const { Key } = command.input;
+      }
+      return Promise.resolve();
+    },
+  };
 
-          delete objects[Key];
-
-          return Promise.resolve({});
-        }
-        if (command instanceof GetObjectCommand) {
-          const { Key } = command.input;
-
-          if (objects[Key]) {
-            const stream = new Readable();
-            stream.push('hello world');
-            stream.push(null); // End of stream
-
-            return {
-              Body: stream,
-              AcceptRanges: 'bytes',
-              ContentLength: 36,
-              ContentRange: 'bytes 0-35/36',
-              ContentType: 'text/plain',
-            };
-          } else {
-            return Promise.reject(new Error('Not found'));
-          }
-        }
-        return Promise.resolve();
-      },
-    };
-  }
   return s3;
 }
 
