dd95fe0b43fb44b7810c106f6071ca254226905c
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2024-10-22 15:41:22 +0200

    fix testing env var

--- spec/support/helper.js
+++ spec/support/helper.js
@@ -6,9 +6,6 @@ const { startServer, stopServer, reconfigureServer } = require('./server');
 jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.TESTING_TIMEOUT || "360000";
 jasmine.getEnv().addReporter(new SpecReporter());
 
-// Setup env vars
-process.env.TESTING = "true";
-
 beforeAll(async () => {
   await startServer();
 });
